89169b65fdb22f8163867d08dd36084982a31848
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-09-21 19:09:50 +0600

    destructuring props title atribute

--- js/countries.js
+++ js/countries.js
@@ -4,18 +4,35 @@ const  loadCountries = () => {
     .then(data => displayCountries(data));
 }
 const displayCountries =  countries => {
-    // console.log(countries);
+    console.log(countries);
     const allCountriesHTML = countries.map(country => getCountryHTML(country));
     // console.log(allCountriesHTML[0]);
     const container = document.getElementById('countries');
     container.innerHTML = allCountriesHTML.join(' ');
 }
 
-const getCountryHTML = country => {
+// const getCountryHTML = country => {
+//     //  option  1 
+//    const {name, flags} = country;
+//     return `
+//        <div>
+//           <h2>${country.name.common}</h2>
+//           <img src="${country.flags.png}">
+//        </div>
+    
+//     `
+// }
+const getCountryHTML = (country) => {
+    //  orginal
+    // distrutaring
+    const {name,flags,area,region} = country;
     return `
        <div>
-          <h2>${country.name.common}</h2>
-          <img src="${country.flags.png}">
+          <h2>${name.common}</h2>
+          <img src="${flags.png}">
+          <p>Area: ${area}</p>
+          <p>Region: ${region}</p>
+
        </div>
     
     `
