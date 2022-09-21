65253589a99c82a0cda286585c51cc64129979ae
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-09-21 16:14:32 +0600

    data loaded

--- js/countries.js
+++ js/countries.js
@@ -4,14 +4,18 @@ const  loadCountries = () => {
     .then(data => displayCountries(data));
 }
 const displayCountries =  countries => {
-    console.log(countries);
-    const allCountriesHTML = countries.map(country => )
+    // console.log(countries);
+    const allCountriesHTML = countries.map(country => getCountryHTML(country));
+    // console.log(allCountriesHTML[0]);
+    const container = document.getElementById('countries');
+    container.innerHTML = allCountriesHTML.join(' ');
 }
 
 const getCountryHTML = country => {
     return `
        <div>
           <h2>${country.name.common}</h2>
+          <img src="">
        </div>
     
     `
